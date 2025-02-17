31889ffc7e8fc492b9c57246b711439b3a69781e
unknown <philipterryt@gmail.com>
2025-02-16 18:53:09 -0600

    implement core functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Main {
 
     /// CONSTANTS
+    static final private Scanner cinScan = new Scanner(System.in);
     static final private String rootPath = ".";
     static final private String masterFileName = "masterfile.txt";
     static final private ArrayList<String> videoExtensions = new ArrayList<>(List.of("mkv", "mov", "mp4", "avi"));
@@ -46,12 +47,31 @@ public class Main {
 //        return new FileHandlerResult(true, null); // TODO: FIX
 //    }
 
-    private boolean saveFile(String filePath){ return true; }
+    private static boolean saveFile(String filepath, Iterable<String> filecontent){
+        try {
+            Files.write(Path.of(filepath), filecontent);
+            System.out.println("Content written to: " + filepath);
+            return true;
+        }
+        catch (IOException e) {
+            System.out.println("FAILED to write to: " + filepath);
+            System.out.println("ERROR: " + e);
+            return false;
+        }
+    }
+
 
     ////
     ///
-    private TreeSet<String> getMasterList(){
+    private static TreeSet<String> getMasterList(){
         TreeSet<String> masterList = new TreeSet<>();
+        String fname = rootPath + "/" + masterFileName;
+        try {
+            masterList.addAll(Files.readAllLines(Path.of(fname)));
+        }
+        catch (IOException e) {
+            System.out.println("No masterlist, using new container.");
+        }
         return masterList;
     }
 
@@ -67,7 +87,6 @@ public class Main {
                     currentFileList.add(file);
                 }
             }
-
         }
         return currentFileList;
     }
@@ -84,16 +103,126 @@ public class Main {
         return true;
     }
 
+    private static LinkedHashMap<String, String> getVideoInfo(String filename) {
+        LinkedHashMap<String, String> itemInfo = new LinkedHashMap<>();
+        System.out.println("We'll need to collect information for '"+ filename + "' file's info file.");
+        for (var entry : permittedValues.entrySet()) {
+            String key = entry.getKey();
+            if (key.equals("Filename")) { continue; }
+            String[] values = entry.getValue();
+            if (values.length > 1) {
+                System.out.println("Please enter '" + key + "' of the film.");
+                System.out.println("Choose a number from the following choices.");
+                for (int i = 0; i < values.length; i++) {
+                    int displayInt = i + 1;
+                    System.out.println("(" + displayInt + ") " + values[i]);
+                }
+                while (true) {
+                    if (cinScan.hasNextInt()){
+                        int nextInt = cinScan.nextInt() - 1;
+//                        System.out.println("Next Int: " + nextInt);
+                        cinScan.nextLine();
+                        if (nextInt >= 0 && nextInt <= values.length){
+                            itemInfo.put(key, values[nextInt]);
+                            System.out.println("Item Selected: " + values[nextInt]);
+                            break;
+                        }
+                        else {
+                            System.out.println("Invalid input. Please enter a number 1 through " + values.length);
+                        }
+                    }
+                    else {
+                        System.out.println("Invalid input. Please enter an integer.");
+                    }
+                }
+            }
+            else {
+                System.out.println("Please enter '" + key + "' of the film.");
+                String response = cinScan.nextLine();
+                itemInfo.put(key, response);
+            }
+        }
+        return itemInfo;
+    }
 
+
+    private static void promptDelete(String filepath, TreeSet<String> masterList) {
+        ArrayList<String> allowedResponses = new ArrayList<>(Arrays.asList("y", "yes", "n", "no"));
+        System.out.println("'" + filepath + "' was found in the master list but no longer exists." +
+                " Would you like to remove it? (y/n)");
+        while (true) {
+            String response = cinScan.nextLine().toLowerCase();
+            if (allowedResponses.contains(response)){
+                if (response.equals("y") || response.equals("yes")) {
+                    masterList.remove(filepath);
+                    try {
+                        Files.delete(Path.of(rootPath + filepath + ".txt"));
+                        System.out.println("File deleted: " + filepath + ".txt");
+                    } catch (IOException e) {
+                        System.out.println("Couldn't delete '" + filepath + "': File missing");
+                    }
+                    break;
+                }
+                else {
+                    System.out.println("File skipped for deletion.");
+                    break;
+                }
+            }
+            else {
+                System.out.println("Please enter a valid response: y, yes, n, or no.");
+            }
+        }
+    }
 
 
 
 
     public static void main(String[] vars) {
 
-        HashSet<String> fList = new HashSet<>(getFileList());
-        System.out.println(fList.size());
-        fList.forEach(System.out::println);
+        getVideoInfo("SomeMovie.mov");
+        getMasterList();
+        runTests(
+                new TestFilelist(),
+                new TestSaveFile()
+        );
+    }
+
+    interface Test {
+        boolean test();
+        default void run() {
+            String className = this.getClass().getSimpleName();
+            if (test()) {
+                System.out.println("TEST PASSED: " + className);
+            }
+            else {
+                System.out.println("TEST FAILED: " + className);
+            }
+        }
+        
+
+    }
+
+    public static void runTests(Test... tests) {
+        for (Test t : tests) {
+            t.run();
+        }
+    }
+
+    static class TestFilelist implements Test {
+        HashSet<String> testCase = new HashSet<>(Arrays.asList("movie_one(2010).mp4","movie_three(2022).mkv", "movie_two(1997).mov"));
+        @Override
+        public boolean test(){
+            return getFileList().equals(testCase);
+            }
+    }
+
+    static class TestSaveFile implements Test {
+        @Override
+        public boolean test() {
+            ArrayList<String> testCase = new ArrayList<>(List.of("This", "Function", "Works"));
+            return saveFile(rootPath + "/testSave.txt",  testCase);
+        }
     }
 
 }
+
